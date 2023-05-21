83a64702a69582659dbbc065a3b6c30ffd89fb66
Dzianis Savastsiuk <dzianis_savastsiuk@epam.com>
2023-05-21 22:26:21 +0300

    Use debounce from lodash library to prevent calling function on scroll too often.

--- main-page-functions.js
+++ main-page-functions.js
@@ -2,6 +2,7 @@
 const couponContainer = document.querySelector('.coupons-container');
 const categoryContainer = document.querySelector('.category-container');
 const toTopButton = document.querySelector('.to-top');
+const couponTemplate = document.querySelector('#coupon-template');
 
 const searchBar = document.forms['search-form'].querySelector('input');
 let page = '1';
@@ -21,13 +22,15 @@ searchBar.addEventListener('keyup', function(e){
 const baseUrl = 'http://localhost:5000/rest/certificates/';
 const baseImageUrl = 'https://api.unsplash.com/photos';
 async function getData(page){
-    let pageParam = '?page='+page;
+    let certificatesPerPage = 10;
+    let pageParam = '?page='+page + '&pageSize='+certificatesPerPage;
     const response = await fetch(baseUrl+pageParam);
     const data = await response.json();
     return data;
 }
 async function getImage(page){
-    let imageUrlPaginated = baseImageUrl + `?page=${page}`;
+    let imagesPerPage = 10;
+    let imageUrlPaginated = baseImageUrl + `?page=${page}` + "&X-Per-Page="+imagesPerPage;
     const response = await fetch(imageUrlPaginated, {
         method: 'get',
         headers:{
@@ -41,34 +44,35 @@ async function getImage(page){
     return data;
 }
 
-async  function loadCouponList(page){
+
+async function loadCouponList(page){
     const data = await getData(page);
-    let couponsList = data._embedded.certificateDtoList;
-    couponsList.forEach(async function callBack(element, index) {
-        const data = await getImage(page);
-        console.log(data);
-        let clonedContainer = couponContainer.querySelector('.coupon').cloneNode(true);
-        
-        clonedContainer.firstElementChild.firstElementChild.src = data[index].urls.small;
+    const images = await getImage(page);
+    let couposList = data._embedded.certificateDtoList;
+    
+
+    couposList.forEach(function callback(element, index){
+        let clonedContainer = couponTemplate.content.querySelector('.coupon').cloneNode(true);
+        clonedContainer.firstElementChild.firstElementChild.src = images[index].urls.small;
         clonedContainer.querySelector('.coupon-info-1 p').innerHTML = element.name;
         clonedContainer.querySelector('.coupon-price p').innerHTML = element.price + '$';
         clonedContainer.querySelector('.coupon-info-2').lastElementChild.innerHTML = `expares in ${element.duration} days`;
         couponContainer.appendChild(clonedContainer);
-
     })
-    
-} 
-loadCouponList(page);
+}
 
-window.addEventListener('scroll', () => {
-    
-    const documentRect = couponContainer.getBoundingClientRect();
-    
-    if(documentRect.bottom < document.documentElement.clientHeight + 150){
+
+
+loadCouponList(page);
+function handleScroll(){
+    const documentRect = couponContainer.getBoundingClientRect();   
+    if(documentRect.bottom < document.documentElement.clientHeight + 100){
         page++;
-        loadCouponList(page);
+        loadCouponList(page);     
     }
-})
+}
+
+window.addEventListener('scroll', _.debounce(handleScroll, 500))
 
 window.addEventListener('scroll', () => {
     if(window.pageYOffset > 100){
@@ -77,4 +81,30 @@ window.addEventListener('scroll', () => {
         toTopButton.classList.remove('active');
     }
 })
+
+function reloadData(page1, callback){
+    console.log(page1, 2 < page1);
+    for(i = 2; i < page1; i++){
+        loadCouponList(page1);
+    }
+    callback();
+}
+document.addEventListener("DOMContentLoaded", () => {    
+    var scrollpos = localStorage.getItem('scrollpos');
+    
+    
+    if (scrollpos){
+        reloadData(localStorage.getItem('page'), function() {
+            window.scrollTo(0, scrollpos);
+            localStorage.removeItem('scrollpos');
+        })   
+    }
+    
+});
+
+
+window.onbeforeunload = function(e) {
+    localStorage.setItem('scrollpos', window.scrollY);
+    localStorage.setItem('page', page);
+};
 
